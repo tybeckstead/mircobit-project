2dc6e849b7c0a9fc65b6f8e267205679fe52d1cd
Tyler Beckstead <51000520+tybeckstead@users.noreply.github.com>
2021-03-11 17:29:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,12 +20,29 @@ function introAnimation () {
         . . . . .
         `)
 }
+function musicBox () {
+    music.setTempo(120)
+    music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Forever)
+    basic.showIcon(IconNames.Heart)
+}
 input.onButtonPressed(Button.A, function () {
 	
 })
-music.playMelody("G A F - E D C C ", 170)
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    musicBox()
+})
+music.playMelody("G A F - E D C C ", 230)
 introAnimation()
-basic.showString("Preas Logo")
+basic.showString("Press Logo")
 basic.forever(function () {
 	
 })
