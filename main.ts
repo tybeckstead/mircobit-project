6eebc9da3fdd543f87fbfa54a6677bb4b1142ac3
Tyler Beckstead <51000520+tybeckstead@users.noreply.github.com>
2021-03-11 22:21:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,4 @@
-function turnOnMelody () {
-    music.playMelody("E B C5 A B G A F ", 150)
-    basic.showString("Welcome!")
-}
-turnOnMelody()
-basic.forever(function () {
+function tempFunct () {
     if (input.temperature() <= 22) {
         basic.showString("" + (input.temperature()))
         basic.showString("It's Cold")
@@ -11,4 +6,12 @@ basic.forever(function () {
         basic.showString("" + (input.temperature()))
         basic.showString("It's Warm")
     }
+}
+function turnOnMelody () {
+    music.playMelody("E B C5 A B G A F ", 150)
+    basic.showString("Welcome!")
+}
+turnOnMelody()
+basic.forever(function () {
+    tempFunct()
 })
