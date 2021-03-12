a0d923aa19e51a3039899c0dd5177e75b7c0ed4b
Tyler Beckstead <51000520+tybeckstead@users.noreply.github.com>
2021-03-11 22:04:51 -0500

    Temperature and Message Function

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function turnOnMelody () {
+    music.playMelody("E B C5 A B G A F ", 150)
+    basic.showString("Welcome!")
+}
+turnOnMelody()
 basic.forever(function () {
-	
+    if (21 < input.temperature()) {
+        basic.showString("" + (input.temperature()))
+        basic.showString("It's Cold")
+    } else {
+        basic.showString("" + (input.temperature()))
+        basic.showString("It's Warm")
+    }
 })
